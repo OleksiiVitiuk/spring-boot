1243b7acf90bfa02b6e1cd3d45a94eefc4a01cff
Oleksii Vitiuk <vitiuk.oleksii@gmail.com>
2025-02-20 14:32:02 +0200

    1.3

--- src/main/java/bookstore/repository/BookRepository.java
+++ src/main/java/bookstore/repository/BookRepository.java
@@ -2,9 +2,7 @@ package bookstore.repository;
 
 import bookstore.entity.Book;
 import java.util.List;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BookRepository {
     Book save(Book book);
 
