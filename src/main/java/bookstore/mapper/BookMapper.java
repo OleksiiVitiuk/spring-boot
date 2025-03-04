517aeb5e8d8abf6e82ffccff1f34aeed49a88d01
Oleksii Vitiuk <vitiuk.oleksii@gmail.com>
2025-03-04 19:17:50 +0200

    1.6 Data JPA

--- src/main/java/bookstore/mapper/BookMapper.java
+++ src/main/java/bookstore/mapper/BookMapper.java
@@ -5,10 +5,15 @@ import bookstore.dto.BookDto;
 import bookstore.dto.CreateBookRequestDto;
 import bookstore.entity.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto createBookRequestDto);
+
+    @Mapping(target = "id", ignore = true)
+    void updateModel(CreateBookRequestDto createBookRequestDto, @MappingTarget Book book);
 }
