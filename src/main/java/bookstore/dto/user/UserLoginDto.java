1b4f8f78dda4910742d1c473a07598e5b01ab9ca
Oleksii Vitiuk <vitiuk.oleksii@gmail.com>
2025-03-29 16:47:17 +0200

    1.10 JWT support

---
+++ src/main/java/bookstore/dto/user/UserLoginDto.java
@@ -0,0 +1,14 @@
+package bookstore.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UserLoginDto {
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/bookstore/dto/user/UserResponseLoginDto.java
@@ -0,0 +1,4 @@
+package bookstore.dto.user;
+
+public record UserResponseLoginDto(String token) {
+}
